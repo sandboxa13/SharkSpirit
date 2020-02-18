9efc2a5bb8ca9cc78963c8acfd86750528d4568c
Dmitry <dima_nikishin_84@mail.ru>
2020-02-19 00:06:11 +0300

    added fast collection

---
+++ src/SharkSpirit.Core/Collections/ArrayHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace SharkSpirit.Engine
+{
+    public struct ArrayHelper<T>
+    {
+        /// <summary>
+        /// An empty array of the specified <see cref="T"/> element type.
+        /// </summary>
+        public static readonly T[] Empty = new T[0];
+    }
+}
