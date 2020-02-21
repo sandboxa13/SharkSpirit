e6c254a971f2a58156a44d1a74a0ec1749afd1f1
Dmitry <dima_nikishin_84@mail.ru>
2020-02-22 00:36:35 +0300

    added constant buffer bindable and desc factory

---
+++ src/SharkSpirit.RenderFramework.DirectX/Pipeline/ConstantBufferBindable.cs
@@ -0,0 +1,21 @@
+using SharkSpirit.RenderFramework.DirectX.Pipeline.Factories;
+using SharpDX.Direct3D11;
+
+namespace SharkSpirit.RenderFramework.DirectX.Pipeline
+{
+    public class ConstantBufferBindable<T> : BindableBase where T : struct 
+    {
+        public ConstantBufferBindable(IDevice device) : base(device)
+        {
+            var cbd = ConstantBufferDescriptionFactory.CreateConstantBufferDescription();
+
+            ConstantBuffer = new Buffer(device.GetDevice(), cbd);
+        }
+        protected  Buffer ConstantBuffer { get; private set; }
+        
+        protected override void Bind()
+        {
+            
+        }
+    }
+}
---
+++ src/SharkSpirit.RenderFramework.DirectX/Pipeline/Factories/ConstantBufferDescriptionFactory.cs
@@ -0,0 +1,22 @@
+using SharkSpirit.Graphics;
+using SharpDX;
+using SharpDX.Direct3D11;
+
+namespace SharkSpirit.RenderFramework.DirectX.Pipeline.Factories
+{
+    public static class ConstantBufferDescriptionFactory
+    {
+        public static BufferDescription CreateConstantBufferDescription()
+        {
+            var cbd = new BufferDescription
+            {
+                Usage = ResourceUsage.Default,
+                SizeInBytes = Utilities.SizeOf<ConstantBuffer>(),
+                BindFlags = BindFlags.ConstantBuffer,
+                CpuAccessFlags = CpuAccessFlags.None,
+            };
+
+            return cbd;
+        }
+    }
+}
