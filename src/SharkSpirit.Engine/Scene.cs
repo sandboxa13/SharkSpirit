5781b30f31833e9f0a5ef2566dd2c787ced4f73a
Dmitry <dima_nikishin_84@mail.ru>
2020-02-22 01:44:32 +0300

    update scene

--- src/SharkSpirit.Engine/Scene.cs
+++ src/SharkSpirit.Engine/Scene.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.Generic;
 using DryIoc;
 using SharkSpirit.Core;
-using SharkSpirit.RenderEngine;
+using SharkSpirit.Core.Collections;
+using SharkSpirit.Engine.Systems;
+using SharkSpirit.RenderFramework.DirectX;
 
 namespace SharkSpirit.Engine
 {
@@ -14,21 +16,28 @@ namespace SharkSpirit.Engine
 
         public RenderSystem RenderSystem { get; private set; }
 
-        public List<Entity> Entities { get; private set; }
+        public FastCollection<Entity> Entities { get; private set; }
 
+        public void Draw()
+        {
+            RenderSystem.Draw();
+        }
+        
         public void AddEntity(Entity entity)
         {
             Entities.Add(entity);
+            RenderSystem.EntityRenderProcessor.AddRenderObject(entity, new RenderObject());
         }
 
         public void RemoveEntity(Entity entity)
         {
             Entities.Remove(entity);
+            RenderSystem.EntityRenderProcessor.RemoveRenderObject(entity);
         }
 
         private void Initialize(IContainer container)
         {
-            RenderSystem.CreateRenderSystem(container);
+            RenderSystemFactory.CreateRenderSystem(container);
         }
     }
 }
