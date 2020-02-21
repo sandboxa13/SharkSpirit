7d71ef7a761e0dbcb045457ee09d78e8a57c18a2
Dmitry <dima_nikishin_84@mail.ru>
2020-02-22 01:44:44 +0300

    move render system to systems

---
+++ src/SharkSpirit.Engine/Systems/RenderSystem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using DryIoc;
+using SharkSpirit.Core;
+using SharkSpirit.RenderFramework.DirectX;
+
+namespace SharkSpirit.Engine.Systems
+{
+    public class RenderSystem : ComponentBase
+    {
+        internal RenderSystem(IDevice device)
+        {
+            Device = device;
+            EntityRenderProcessor = new EntityRenderProcessor();
+        }
+
+        public IDevice Device { get; }
+        public EntityRenderProcessor EntityRenderProcessor { get; private set; }
+
+        public void Draw()
+        {
+            EntityRenderProcessor.Draw();
+        }
+    }
+}
