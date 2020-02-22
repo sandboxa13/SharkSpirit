037dfd65c033443dfd3e1575fd239f80443c8642
Dmitry <dima_nikishin_84@mail.ru>
2020-02-22 23:45:28 +0300

    added camera component

---
+++ src/SharkSpirit.Engine/Components/CameraComponent.cs
@@ -0,0 +1,29 @@
+using SharpDX;
+
+namespace SharkSpirit.Engine.Components
+{
+    public class CameraComponent : EntityComponent
+    {
+        public Matrix ViewMatrix;
+        
+        public void Update()
+        {
+            var position = new Vector3(Entity.TransformComponent.Position.X, Entity.TransformComponent.Position.Y, Entity.TransformComponent.Position.Z);
+
+            var lookAt = new Vector3(0, 0, 1);
+
+            var pitch = Entity.TransformComponent.Position.X * 0.0174532925f;
+            var yaw = Entity.TransformComponent.Position.Y * 0.0174532925f; 
+            var roll = Entity.TransformComponent.Position.Z * 0.0174532925f; 
+
+            var rotationMatrix = Matrix.RotationYawPitchRoll(yaw, pitch, roll);
+
+            lookAt = Vector3.TransformCoordinate(lookAt, rotationMatrix);
+            var up = Vector3.TransformCoordinate(Vector3.UnitY, rotationMatrix);
+
+            lookAt = position + lookAt;
+
+            ViewMatrix = Matrix.LookAtLH(position, lookAt, up);
+        }
+    }
+}
