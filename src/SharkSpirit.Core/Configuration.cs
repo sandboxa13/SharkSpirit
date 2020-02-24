6ac5790b161fd39af7ab41e3645f3baf20e9333a
Dmitry <dima_nikishin_84@mail.ru>
2020-02-24 23:10:30 +0300

    update configuration

--- src/SharkSpirit.Core/Configuration.cs
+++ src/SharkSpirit.Core/Configuration.cs
@@ -3,10 +3,13 @@
     public class Configuration : IConfiguration
     {
         public EngineEditorType EngineEditorType { get; set; }
+        public string PathToShaders { get; set; }
     }
 
     public interface IConfiguration
     {
         EngineEditorType EngineEditorType { get; set; }
+        
+        string PathToShaders { get; set; }
     }
 }
