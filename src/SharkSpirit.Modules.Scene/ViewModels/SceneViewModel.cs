d58a8d02abc29ea70590babc48841547c6b93161
Dmitry <dmitry.nikishin.84@gmail.com>
2020-08-10 21:18:38 +0300

    disable rotation for scene

--- src/SharkSpirit.Modules.Scene/ViewModels/SceneViewModel.cs
+++ src/SharkSpirit.Modules.Scene/ViewModels/SceneViewModel.cs
@@ -30,7 +30,7 @@ namespace SharkSpirit.Modules.Scene.ViewModels
             _game = _container.Resolve<Game>();
 
             Task.Run(async () => { await _game.Scene.AddEntityAsync(new Entity(new Vector3(0, 0, 0), container, $"Point Light"));});
-            // Task.Run(async () => { await _game.Scene.LoadModelAsync("Sponza\\sponza.obj",1.0f / 40.0f);});
+            Task.Run(async () => { await _game.Scene.LoadModelAsync("Sponza\\sponza.obj",1.0f / 40.0f);});
             Task.Run(async () => { await _game.Scene.LoadModelAsync("nanosuit.obj", 1f, true);});
         }
 
