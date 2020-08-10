ce11e2e0a6024727863d0f75b4db4a772da5f88c
Dmitry <dmitry.nikishin.84@gmail.com>
2020-08-10 23:08:12 +0300

    fix scene selected items

--- src/SharkSpirit.Modules.SceneInspector/ViewModels/SceneItemInspectorViewModel.cs
+++ src/SharkSpirit.Modules.SceneInspector/ViewModels/SceneItemInspectorViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using ReactiveUI.Fody.Helpers;
 using SharkSpirit.Engine;
 using SharkSpirit.Modules.Core.ViewModels;
@@ -8,14 +9,26 @@ namespace SharkSpirit.Modules.SceneInspector.ViewModels
 {
     public class SceneItemInspectorViewModel : ViewModelBase
     {
+        private readonly Dictionary<Guid, SelectedSceneItemViewModel> _selectedSceneItemViewModels;
+        
         public SceneItemInspectorViewModel(SceneGraphManager sceneGraphManager)
         {
+            _selectedSceneItemViewModels = new Dictionary<Guid, SelectedSceneItemViewModel>();
+            
             sceneGraphManager.SelectedEntityChanged.Subscribe(UpdateSelectedItem);
         }
 
         private void UpdateSelectedItem(Entity entity)
         {
-            SelectedSceneItemViewModel = new SelectedSceneItemViewModel(entity);
+            if (_selectedSceneItemViewModels.TryGetValue(entity.Id, out var viewModel))
+            {
+                SelectedSceneItemViewModel = viewModel;
+            }
+            else
+            {
+                SelectedSceneItemViewModel = new SelectedSceneItemViewModel(entity);
+                _selectedSceneItemViewModels.Add(entity.Id, SelectedSceneItemViewModel);
+            }
         }
 
         [Reactive] public SelectedSceneItemViewModel SelectedSceneItemViewModel { get; private set; }
