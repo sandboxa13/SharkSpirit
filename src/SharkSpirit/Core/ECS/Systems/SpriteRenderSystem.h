10e1dfadbc13a2a3bb5d489d2a3d5d1abb59d342
Dmitry <dmitry.nikishin.84@gmail.com>
2021-10-30 20:07:56 +0300

    first sprite

--- src/SharkSpirit/Core/ECS/Systems/SpriteRenderSystem.h
+++ src/SharkSpirit/Core/ECS/Systems/SpriteRenderSystem.h
@@ -10,11 +10,12 @@ namespace SharkSpirit
 
 		void static render_sprite(graphics_manager* graphics, sprite_component& sprite)
 		{
+			sprite.worldMatrix = DirectX::XMMatrixScaling(1280, 720, 1.0f) * DirectX::XMMatrixRotationRollPitchYaw(0, 0, 0)* DirectX::XMMatrixTranslation(0 + 1280 / 2.0f, 0 + 720 / 2.0f, 0);
 			graphics->get_device_context()->IASetInputLayout(sprite.vertexshader_2d.GetInputLayout());
 			graphics->get_device_context()->PSSetShader(sprite.pixelshader_2d.GetShader(), NULL, 0);
 			graphics->get_device_context()->VSSetShader(sprite.vertexshader_2d.GetShader(), NULL, 0);
 
-			DirectX::XMMATRIX wvpMatrix = sprite.worldMatrix;
+			DirectX::XMMATRIX wvpMatrix = sprite.worldMatrix * DirectX::XMMatrixOrthographicOffCenterLH(0.0f, 1280, 720, 0.0f, 0.0f, 1.0f);
 			graphics->VSSetConstantBuffers(0, 1, sprite.cb_vs_vertexshader_2d->GetAddressOf());
 			sprite.cb_vs_vertexshader_2d->data.wvpMatrix = wvpMatrix;
 			sprite.cb_vs_vertexshader_2d->ApplyChanges();
